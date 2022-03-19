4769f00126bfb61c23fa29f594fbdb14ef0b1fc7
name <dhy9108@gmail.com>
2022-03-19 14:22:39 +0900

    init add

---
+++ src/main/java/com/idolticketing/idolticketing/IdolTicketingApplication.java
@@ -0,0 +1,13 @@
+package com.idolticketing.idolticketing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdolTicketingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdolTicketingApplication.class, args);
+	}
+
+}
