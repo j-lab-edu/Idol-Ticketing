51e9d51196034dec51216cc2620d4ba66e206569
name <dhy9108@gmail.com>
2022-05-05 21:20:59 +0900

    add dev branch

---
+++ src/main/java/com/idolticketing/idolticketing/dto/UserResponseDTO.java
@@ -0,0 +1,25 @@
+package com.idolticketing.idolticketing.dto;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
+public class UserResponseDTO {
+
+    private String userId;
+    private String name;
+    private int code;
+    private String message;
+
+    public UserResponseDTO(){
+    }
+
+    public UserResponseDTO(String userId, String name, int code, String message){
+
+        this.userId = userId;
+        this.name = name;
+        this.code = code;
+        this.message = message;
+    }
+}
